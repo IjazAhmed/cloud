58d07baebbb18daeb5bb6963698ada8e7e706505
Ijaz Ahmed <ijaz.uma@gmail.com>
2025-11-02 20:24:43 +0000

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
             <p>Join our community today to access exclusive deals and features!</p>
             
             <!-- Simple link to signup page -->
-            <a href="signup.php" class="signup-link">Create Account</a>
+            <a href="sign.php" class="signup-link">Create Account</a>
         </div>
     </div>
 </body>
