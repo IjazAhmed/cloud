6d293bf353bc3019c1fc59acfdb9a12dd903c357
Ijaz Ahmed <ijaz.uma@gmail.com>
2025-11-02 19:14:49 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@ $serverName = "tcp:mycardiffmet.database.windows.net,1433";
 $connectionOptions = array(
     "Database" => "myDatabase",
     "Uid" => "myadmin", 
-    "PWD" => "abcdefgh0!",
+    "PWD" => "Abcdefgh0!",
     "Encrypt" => 1,
     "TrustServerCertificate" => 0
 );
