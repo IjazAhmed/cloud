36902380d0242717fef9cfbfd2f6f5a01e693bd6
Ijaz Ahmed <ijaz.uma@gmail.com>
2025-11-02 20:46:28 +0000

    Update signup link text to 'Create Your Account'

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
             <p>Join our community today to access exclusive deals and features!</p>
             
             <!-- Simple link to signup page -->
-            <a href="register.html" class="signup-link">Create Account</a>
+            <a href="register.html" class="signup-link">Create Your Account</a>
         </div>
     </div>
 </body>
